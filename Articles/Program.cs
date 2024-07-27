bb516fbe645571279679d4c72531357b9c3a5d44
ali.momeni11.am <ali.momeni11.am@gmail.com>
2024-07-27 16:29:53 +0330

    Add ssl to article server

--- Articles/Program.cs
+++ Articles/Program.cs
@@ -16,6 +16,7 @@ namespace Articles
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
