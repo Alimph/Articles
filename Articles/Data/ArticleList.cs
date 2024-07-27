51ea0b8521d35850b9a012177d2e48a321391fe4
ali.momeni11.am <ali.momeni11.am@gmail.com>
2024-07-27 15:30:15 +0330

    Create ArticleApi

---
+++ Articles/Data/ArticleList.cs
@@ -0,0 +1,32 @@
+﻿using Articles.Protos;
+using Bogus;
+
+namespace Articles.Data
+{
+    public class ArticleList
+    {
+        public ArticleList()
+        {
+            if (Articles is null)
+            {
+                var faker = new Faker("fa");
+                for (int i = 0; i < 10; i++)
+                {
+                    Articles = new List<Article>
+                    {
+                        new Article
+                        {
+                           Author = faker.Name.FullName(),
+                           DateOfRelease = faker.Date.Locale,
+                           Text = faker.Lorem.Text(),
+                           Title = faker.Name.JobTitle(),
+                           Type = ArticleType.Text,
+                        }
+                    };
+                }
+            }
+        }
+        public static List<Article> Articles { get; set; }
+
+    }
+}
---
+++ Articles/Services/ArticleApi.cs
@@ -0,0 +1,29 @@
+﻿using Articles.Protos;
+using Grpc.Core;
+
+namespace Articles.Services
+{
+    public class ArticleApi : ArticleService.ArticleServiceBase
+    {
+        private readonly ArticleList _articles = new ArticleList();
+        public override Task<DefaultResponse> Create(Article request, ServerCallContext context)
+        {
+            _articles.Items.Add(request);
+            return Task.FromResult(new DefaultResponse
+            {
+                Message = "job successful."
+            });
+        }
+
+        public override async Task GetAll(GetArticleCount request, IServerStreamWriter<Article> responseStream, ServerCallContext context)
+        {
+            var i = 0;
+            while (!context.CancellationToken.IsCancellationRequested && i < 50)
+            {
+                await Task.Delay(1000);
+                await responseStream.WriteAsync(_articles.Items[i]);
+                i++;
+            }
+        }
+    }
+}
